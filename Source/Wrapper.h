b04141301acd27295f9a0e04d718af8db93df7f8
lily-cdev <therealarcticsnow@gmail.com>
2025-10-21 22:16:34 -0700

    fish

--- Source/Wrapper.h
+++ Source/Wrapper.h
@@ -9,6 +9,7 @@
 
 #define OUTPUTNAME "yield.bmp"
 #define DATAOFFSET 62
+#define DEFAULTSIZE 1
 
 extern const unsigned char Black[4];
 extern const unsigned char White[4];
@@ -25,49 +26,7 @@ inline bool Is_Digit(const char Input) {
 	return Input >= '0' && Input <= '9';
 }
 
-inline void Write_BMP(const uint32_t Width, unsigned char* Row) {
-	uint32_t Height = Width / 4;
-	if (Width < 75) {
-		Height = max(75, Height);
-	}
-	uint32_t Row_Size = ((Width + 31) / 32) * 4;
-	uint32_t Image_Size = Row_Size * Height;
-	uint32_t File_Size = Image_Size + DATAOFFSET;
-	uint32_t Data_Offset = DATAOFFSET;
-	FILE* Yield = NULL;
-	int Check = fopen_s(&Yield, OUTPUTNAME, "wb");
-	if (Check != 0) {
-		return;
-	}
-	fwrite("BM", 1, 2, Yield);
-	fwrite(&File_Size, 4, 1, Yield);
-	const uint16_t Zero2 = 0;
-	fwrite(&Zero2, 2, 1, Yield);
-	fwrite(&Zero2, 2, 1, Yield);
-	fwrite(&Data_Offset, 4, 1, Yield);
-	const uint32_t DIB_Size = 40;
-	fwrite(&DIB_Size, 4, 1, Yield);
-	fwrite(&Width, 4, 1, Yield);
-	fwrite(&Height, 4, 1, Yield);
-	const uint16_t One2 = 1;
-	fwrite(&One2, 2, 1, Yield);
-	fwrite(&One2, 2, 1, Yield);
-	const uint32_t Zero4 = 0;
-	fwrite(&Zero4, 4, 1, Yield);
-	fwrite(&Image_Size, 4, 1, Yield);
-	const int32_t PPM = 11811;
-	fwrite(&PPM, 4, 1, Yield);
-	fwrite(&PPM, 4, 1, Yield);
-	fwrite(&Zero4, 4, 1, Yield);
-	fwrite(&Zero4, 4, 1, Yield);
-	fwrite(White, 1, 4, Yield);
-	fwrite(Black, 1, 4, Yield);
-	for (int Counter = 0; Counter < Height; Counter++) {
-		fwrite(Row, 1, Row_Size, Yield);
-	}
-	fclose(Yield);
-}
-
+void Write_BMP(const uint32_t Width, unsigned char* Row);
 void Generate39(char* Input);
 void Generate128(char* Input);
 uint8_t GenerateITF(char* Input);
--- readme.md
+++ readme.md
@@ -1,9 +1,9 @@
-<b>CLI FLAGS</b><br>
-"-q" → add a quietzone around the barcode //WARNING: NOT IMPLEMENTED<br>
-"-t" → add text below the barcode //WARNING: NOT IMPLEMENTED<br>
-<br>
-<b>Encoding Types</b><br>
+<b>it would be fun to have a project name</b><br>
+having a desc here would be pretty cool<br>
+<b>Encoding Flags</b><br>
 - "-c39" → barcode39; no checksum<br>
 - "-c128" → barcode128; modulo 103 checksum<br>
 - "-itf" → interleaved 2 of 5; no checksum<br>
 - idk whatever i add next
+<b>Other Flags</b><br>
+- "-q" → insert a quietzone around the barcode
