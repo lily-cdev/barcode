6a745b1d6416eb8cac56bdfce8d569a5e5587e65
lily-cdev <therealarcticsnow@gmail.com>
2025-11-03 09:45:39 -0800

    fish

--- Update.bat
+++ Update.bat
@@ -8,4 +8,4 @@ git config --global user.name "lily-cdev"
 git commit -m "fish"
 git branch -m main
 git remote add origin https://github.com/lily-cdev/barcode.git
-git push -u origin main
+git push -u origin main --force
